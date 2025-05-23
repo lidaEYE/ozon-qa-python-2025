9a46ef92ef40397c9871302e34b352e84e7bc806
Lidiya <l.glazikova@gmail.com>
2025-05-23 10:22:57 +0300

    delete height unit, modify main algorythm

--- func.py
+++ func.py
@@ -11,7 +11,7 @@ from collections import Counter
 url = "https://akabab.github.io/superhero-api/api/all.json"
 response = requests.get(url)
 
-gender = 'male'
+gender = 'female'
 job = 'yes'
 
 if response.status_code == 200:
@@ -20,7 +20,9 @@ if response.status_code == 200:
 else:
     print("Ошибка:", response.status_code)
 
-filtered_heroes = []
+height = 0
+result_hero = 'a'
+
 for hero in heroes:
     if hero["appearance"]["gender"].lower() != gender.lower():
         continue
@@ -28,9 +30,9 @@ for hero in heroes:
     occupation = hero["work"]["occupation"].lower()
 
     if occupation in ["-", "none", "unemployed", ""]:
-        employment_status = "no"
+        employment_status = 'no'
     else:
-        employment_status = "yes"
+        employment_status = 'yes'
 
     if employment_status != job:
         continue
@@ -41,11 +43,19 @@ for hero in heroes:
         
         if "cm" in height_str:
             height_cm = float(height_str.replace("cm", "").strip())
+            if height_cm > height:
+                height = height_cm
+                result_hero = hero
+                continue 
         elif "meters" in height_str:
             height_cm = float(height_str.replace("meters", "").strip()) * 100
+            if height_cm > height:
+                height = height_cm
+                result_hero = hero
+                continue
         else:
             continue
-        filtered_heroes.append((height_cm, hero))
+print(f'конечный герой {result_hero}')
 
 
 # проверка наличия работы
@@ -55,41 +65,10 @@ for hero in heroes:
 # вывод результата
 
 
-occupations = set()
-    
-for hero in heroes:
-    occupation = hero["work"]["occupation"]
-        
-        # Пропускаем пустые значения
-    if not occupation or occupation == "-":
-            continue
-            
-        # Разделяем строку по запятым/точкам с запятой
-    for job in occupation.split(", "):
-        for sub_job in job.split("; "):
-            if sub_job.strip():
-                occupations.add(sub_job.strip())
+
     
     
 
-# Собираем единицы измерения из второго элемента массива height
-units = []
-for hero in heroes:
-    height_data = hero["appearance"]["height"]
-    if isinstance(height_data, list) and len(height_data) >= 2:
-        metric_height = height_data[1]  # Берём второй элемент (например, "188 cm")
-        unit = metric_height.strip()[-2:].lower()  # Последние 2 символа
-        units.append(unit)
 
-        if unit == 'kg':
-            print(hero)
-# метры необходимо перевести в см
-
-
-# Подсчитываем и выводим результат
-units_count = Counter(units)
-print("Статистика единиц измерения роста (метрическая система):")
-for unit, count in units_count.most_common():
-    print(f"{unit} - {count}")
 
 
